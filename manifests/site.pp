41c1d0572f28c772fd6cb0d8b9a8c664e686205c
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2022-12-25 20:52:03 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,12 @@
 node slave1.puppet {
   package { 'httpd':
-    ensure => running,
+    ensure => present,
   }
+}
+
+service { 'httpd':
+  ensure => running,
+  start => '/usr/sbin/httpd start', 
+  stop => '/usr/sbin/httpd stop', 
+  pattern => '/usr/sbin/httpd',
 }
