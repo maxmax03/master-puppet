e0e0d97ad109fdfdf49677b346e9afb1c0650b53
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2022-12-29 23:24:44 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,14 @@
 node minecraft.puppet {
-  package { 'nginx':
-    ensure => present,
-  }
   package { 'mc':
     ensure => present,
   }
+  package { 'ngnix':
+    ensure => present,
+  }
   package { 'httpd':
+    ensure => present,
+  }
+  package { 'php':
     ensure => present,
   }
 }
