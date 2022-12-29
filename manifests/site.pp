1723c6b1c3f8732dd695aaa0bfa7ebc28a12b6d2
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2022-12-29 18:24:03 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,5 @@
 node minecraft.puppet {
   package { 'nginx':
     ensure => present,
+  }
 }
-  
-#  file { '/var/www/html/index.html':
-#   ensure => present,
-#    source => "/vagrant/index.html",
-#  }
-  
-#  service { 'nginx':
-#    ensure => running,
-#    enable => true,
-#  }
