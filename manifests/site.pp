83f6651b8f0ad5faa2565c52c8ff90abbb4626a3
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2022-12-30 02:07:08 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,9 @@
 node minecraft.puppet {
+  include nginx
+    nginx::resource::server { 'slave1.puppet':
+    listen_port => 80,
+    proxy       => 'http://localhost:8080',
+}
   package { 'mc':
     ensure => present,
   }
