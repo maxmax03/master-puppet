eb13383080b666fc590837f013073907090013f4
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2022-12-29 15:49:43 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,39 +1,15 @@
-node slave1.puppet {
-  package { 'httpd':
+node minecraft.puppet {
+  package { 'nginx':
     ensure => present,
   }
   
-  file { '/var/www/html/index.html':
-    ensure => present,
-    source => "/vagrant/index.html",
-  }
+#  file { '/var/www/html/index.html':
+#   ensure => present,
+#    source => "/vagrant/index.html",
+#  }
   
-  service { 'httpd':
-    ensure => running,
-    enable => true,
-  }
+#  service { 'nginx':
+#    ensure => running,
+#    enable => true,
+#  }
 }
-
-node slave2.puppet {
-  package { 'httpd':
-    ensure => present,
-  }
-  
-  package { 'php':
-    ensure => present,
-  }
-   
-  file { '/var/www/html/index.php':
-    ensure => present,
-    source => "/vagrant/index.php",
-  }  
-  
-  service { 'httpd':
-    ensure => running,
-    enable => true,
-  }
-}
-
-
-
-
