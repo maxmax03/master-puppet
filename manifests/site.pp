02a2acd110ecd03b877c8470c68a148617ee307d
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2022-12-30 17:44:29 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,7 @@ node mineserver.puppet {
   class{'nginx': }
   nginx::resource::upstream { 'upstream_app':
     members => [
-      '192.168.50.2:80',
+      'slave1.puppet',
     ],
   }
   nginx::resource::location{'/test':
