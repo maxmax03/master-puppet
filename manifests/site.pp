73bf82c4e217eefeed261dfb685718a3f9c27df8
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2022-12-30 17:41:41 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,5 @@
 node mineserver.puppet {
-  package { 'mc':
-    ensure => present,
-  }
-  package { 'httpd':
-    ensure => present,
-  }
-  package { 'php':
-    ensure => present,
-  }
-
-class{'nginx': }
+  class{'nginx': }
   nginx::resource::upstream { 'upstream_app':
     members => [
       '192.168.50.2:80',
@@ -21,3 +11,12 @@ class{'nginx': }
   }
 } 
 
+# package { 'mc':
+#   ensure => present,
+#  }
+#  package { 'httpd':
+#    ensure => present,
+#  }
+#  package { 'php':
+#    ensure => present,
+#  }
