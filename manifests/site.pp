3d77ce7f15672d0fe55f6d9b6270681036b90285
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2023-01-04 14:49:43 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,25 +1,2 @@
 node mineserver.puppet {
-  include nginx
-  
-  nginx::resource::server { 'static':
-    listen_port => 80,
-    proxy       => 'http://192.168.50.2:80',
-  }
-  
-  nginx::resource::server { 'dynamic':
-    listen_port => 81,
-    proxy       => 'http://192.168.50.3:80',
-  }
-  
-  exec { 'selinux_to_permissive':
-    command     => 'setenforce 0',
-    path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
-    user       => 'root',
-  }
-
-  exec { 'reboot_nginx':
-    command     => 'systemctl restart nginx',
-    path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
-    user => 'root',
-  }
-}
+ 
