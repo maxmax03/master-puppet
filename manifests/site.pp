0fcc8414f00a680e8eaa71a1f2d24c9d9fdeda10
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2023-01-04 16:55:57 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,7 @@ node mineserver.puppet {
  
 include nginx
   
-  #nginx::resource::server { '192.168.50.4':
+ # nginx::resource::server { '192.168.50.4':
     #listen_port => 80,
    # proxy => 'http://192.168.50.3',
   #}
