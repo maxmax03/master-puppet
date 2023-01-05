86f3a9e9d08db18782b3a95a91f884351c326694
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2023-01-05 22:13:08 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,12 +2,12 @@ node master.puppet {
 
 include nginx
 
-nginx::resource::server { '192.168.50.1:80':
+nginx::resource::server { 'static':
   listen_port => 80,
   proxy => 'http://192.168.50.2:80',
   }
 
-nginx::resource::server { '192.168.50.1:81':
+nginx::resource::server { 'dynamic':
   listen_port => 81,
   proxy => 'http://192.168.50.3:80',
   }
