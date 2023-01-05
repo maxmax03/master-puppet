8ced79142d9b64d8d664dd3d049e31a4956b86cf
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2023-01-05 20:24:18 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,13 +2,26 @@ node master.puppet {
 
 include nginx
 
-  nginx::resource::server { '192.168.50.1':
+  nginx::resource::server { 'static':
     listen_port => 80,
     proxy       => 'http://192.168.50.2:80',
   }
 
-  nginx::resource::server { '192.168.50.1:81':
+  nginx::resource::server { 'dynamic':
     listen_port => 81,
     proxy       => 'http://192.168.50.3:80',
+  }
+}
+
+exec { 'selinux_to_permissive':
+  command     => 'setenforce 0',
+  path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+  user       => 'root',
+  }
+
+exec { 'reboot_nginx':
+  command     => 'systemctl restart nginx',
+  path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+  user => 'root',
   }
 }
