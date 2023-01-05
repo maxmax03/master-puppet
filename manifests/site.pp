24e1a8d98234d94d1c1903f17441b91ba83cecb6
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2023-01-05 21:10:57 +0300

    Update site.pp
    worked hmw puppet prt.2 with another branches

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,19 +1,3 @@
-node master.puppet {
-
-include nginx
-
-  nginx::resource::server { '192.168.50.1':
-    listen_port => 80,
-    proxy       => 'http://192.168.50.2:80',
-  }
-
-  nginx::resource::server { '192.168.50.1:81':
-    listen_port => 81,
-    proxy       => 'http://192.168.50.3:80',
-  }
-}
-
-
 node slave1.puppet {
   package { 'httpd':
   ensure => present,
