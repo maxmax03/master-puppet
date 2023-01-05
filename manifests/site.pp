e5a52d022bb4561728a46f889d6b999bd634d13f
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2023-01-05 17:30:51 +0300

    transfer mine br to prod

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 node master.puppet { 
 include nginx
 
-  nginx::resource::server { '192.168.50.4':
+  nginx::resource::server { '192.168.50.1':
     listen_port => 80,
     proxy       => 'http://192.168.50.2:80',
   }
   
-  nginx::resource::server { '192.168.50.4:81':
+  nginx::resource::server { '192.168.50.1:81':
     listen_port => 81,
     proxy       => 'http://192.168.50.3:80',
   }
