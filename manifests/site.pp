c245487ade8ac1b045abb7f12f035a0d928afe04
maxmax03 <90062302+maxmax03@users.noreply.github.com>
2023-01-05 17:19:54 +0300

    transfer mine br to prod

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,20 @@
+node master.puppet { 
+include nginx
+
+  nginx::resource::server { '192.168.50.4':
+    listen_port => 80,
+    proxy       => 'http://192.168.50.2:80',
+  }
+  
+  nginx::resource::server { '192.168.50.4:81':
+    listen_port => 81,
+    proxy       => 'http://192.168.50.3:80',
+  }
+}
+
 node slave1.puppet {
   package { 'httpd':
-    ensure => present,
+  ensure => present,
   }
   
   file { '/var/www/html/index.html':
@@ -34,4 +48,37 @@ node slave2.puppet {
   }
 }
 
+node mineserver.puppet {
+  package {'java-17-openjdk':
+    ensure => installed,
+  }
+
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+
+  file {'/opt/minecraft/eula.txt':
+    content => 'eula=true',
+  }
+
+  file { '/opt/minecraft/server.jar':
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
+    replace => false,
+  }
+
+  file { '/etc/systemd/system/minecraft.service':
+    owner => 'root',
+    group => 'root',
+    mode   => '0644',
+    source => "/vagrant/minecraft.service",
+    ensure => present, 
+    replace => false,
+    }
+
+ ~> service { 'minecraft':
+        ensure => running,
+        enable => true
+   }
+} 
 
